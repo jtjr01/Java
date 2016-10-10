314ab69cc89aada837282389033b8f40e3b2cfef
jtjr01 <jteronesjr@gmail.com>
2016-10-10 11:49:26 -0700

    Second Attempt to make sure changes are updated

--- JavaPlayGround/src/TicTacToe/Main.java
+++ JavaPlayGround/src/TicTacToe/Main.java
@@ -9,22 +9,32 @@ public class Main {
 public static void main(String[] args){
 	Scanner s = new Scanner (System.in);
 	Board b = new Board();
-	String input = "";
 	int turn = 1;
 	int count = 0;
 	System.out.println(b.toString());
 	while(count < 9){
-		System.out.print("Please enter a row,column pair for the location you would like an X: ");
-		input= s.nextLine();
+		if(turn == 1){
+			System.out.print("Please enter a row,column pair for the location you would like an X: ");
+		}
+		else{
+			System.out.print("Please enter a row,column pair for the location you would like an O: ");
+		}
 		String rowColumnString = s.nextLine();
 		String[] position = rowColumnString.split(",");
 		int row = Integer.parseInt(position[0]);
 		int column = Integer.parseInt(position[1]);
 		System.out.println("Row = " + row);  
 		System.out.println("Column = " + column);
-		b.setCell(row, column, Cell.X);
+		if(turn == 1){
+			b.setCell(row, column, Cell.X);
+			turn++;
+		}
+		else{
+			b.setCell(row, column, Cell.O);
+			turn--;
+		}
 		System.out.println(b.toString());
-		b.victory();
+		count++;
 		}
 	s.close();
 	}
